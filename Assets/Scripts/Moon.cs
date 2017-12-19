0e9be16f633b4515e0f1b8479a73cf5ea7d109c5
Daniel Tomas <ticidici@yahoo.es>
2017-12-19 02:27:05 +0100

    Hay lunas repartidas por el escenario. Aún no hay contador ni pasa nada al cogerlas todas.

---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        gameObject.SetActive(false);
+    }
+}
